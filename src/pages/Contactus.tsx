8263d94d0e5fd92109c24ec7699eb087e17196e0
segni <segni.836@gmail.com>
2025-12-24 22:03:42 +0300

    `Updated Contactus page with new icons and social media links`

--- src/pages/Contactus.tsx
+++ src/pages/Contactus.tsx
@@ -1,13 +1,11 @@
-
-import { MapPin, Phone } from "lucide-react"
+import { Facebook, Instagram, MapPin, Phone, Twitter } from "lucide-react";
 
 export function ContactPage() {
-
   return (
     <section id="contact" className="py-24 bg-background">
-          <section className="relative h-[50vh] overflow-hidden">
+      <section className="relative h-[50vh] overflow-hidden">
         <img
-          src="/bride-with-flower-crown-close-up-portrait.jpg"
+          src="/other15.jpg"
           alt="Portfolio hero"
           className="w-full h-full object-cover"
         />
@@ -15,37 +13,95 @@ export function ContactPage() {
       </section>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center my-16 ">
-          <div className="text-xs tracking-[0.3em] uppercase text-primary mb-4">Get In Touch</div>
-          <h2 className="text-4xl md:text-5xl font-serif text-secondary">Enquire Now</h2>
+          <div className="text-xs tracking-[0.3em] uppercase text-primary mb-4">
+            Get In Touch
+          </div>
+          <h2 className="text-4xl md:text-5xl font-serif text-secondary">
+            Enquire Now
+          </h2>
         </div>
 
         <div className="grid md:grid-cols-2 gap-12">
           {/* Contact Information */}
-          <div className="space-y-8">
+          <div className="space-y-8 ">
             <div className="flex items-start space-x-4">
+              <div className="border-2 hover:border-0 border-primary rounded-full p-2 w-8 h-8 flex items-center align-middle justify-center ">
+
               <MapPin className="w-6 h-6 text-primary mt-1" />
+              </div>
               <div>
-                <h3 className="text-xl font-semibold text-secondary mb-2">Location</h3>
-                <p className="text-foreground">Haile Garment</p>
+                <h3 className="text-2xl font-bold text-secondary mb-2">
+                  Location
+                </h3>
+                <p className="text-foreground text-xl">Haile Garment</p>
               </div>
             </div>
 
             <div className="flex items-start space-x-4">
-              <Phone className="w-6 h-6 text-primary mt-1" />
+              <div className="border-2 hover:border-0 border-primary rounded-full p-2 w-8 h-8 flex items-center align-middle justify-center ">
+                 <Phone className="w-6 h-6 text-primary mt-1" />
+              </div>
               <div>
-                <h3 className="text-xl font-semibold text-secondary mb-2">Phone Numbers</h3>
+                <h3 className="text-xl font-semibold text-secondary mb-2">
+                  Phone Numbers
+                </h3>
                 <p className="text-foreground">+251 983 604 770</p>
                 <p className="text-foreground">+251 965 567 323</p>
+              </div>
+            </div>
+            
+            <div className="flex items-start space-x-4">
+              <div className="border-2 hover:border-0 border-primary rounded-full p-2 w-8 h-8 flex items-center align-middle justify-center ">
+
+              <Facebook className="w-6 h-6 text-primary mt-1" />
+              </div>
+              <div>
+                <h3 className="text-xl font-semibold text-secondary mb-2">
+                  Godolias photography
+                </h3>
+               
+              </div>
+            </div>
+               <div className="flex items-start space-x-4">
+              <div className="border-2 hover:border-0 border-primary rounded-full p-2 w-8 h-8 flex items-center align-middle justify-center ">
+
+              <Twitter className="w-6 h-6 text-primary mt-1" />
+              </div>
+              <div>
+                <h3 className="text-xl font-semibold text-secondary mb-2">
+                  Godolias photography
+                </h3>
+               
+              </div>
+            </div>
+                    <div className="flex items-start space-x-4">
+              <div className="border-2 hover:border-0 border-primary rounded-full p-2 w-8 h-8 flex items-center align-middle justify-center ">
+
+              <Instagram className="w-6 h-6 text-primary mt-1" />
+              </div>
+              <div>
+                <h3 className="text-xl font-semibold text-secondary mb-2">
+                  Godolias photography
+                </h3>
+               
               </div>
             </div>
           </div>
 
           {/* Image */}
-          <div className="relative h-[500px]">
-            <img src="/wedding-ceremony-outdoor-nature-path.jpg" alt="Wedding ceremony" className="w-full h-full object-cover" />
+          <div className="relative h-[500px] ">
+            {/* <img src="/shemglinna1.jpg" alt="Wedding ceremony" className="w-full h-full object-cover" /> */}
+            <iframe
+           src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d493.3122306201046!2d36.35624725218623!3d8.450869920393197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMjcnMDMuNSJOIDM2wrAyMScyNC4yIkU!5e0!3m2!1sen!2set!4v1766602223517!5m2!1sen!2set"
+
+              className="w-full h-[450px] rounded-xl border-0"
+              loading="lazy"
+
+            />
+           
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
